ccc57b91fc827158c932d35a49f76a3fe1adcc88
Via Perez <viacamilleperez@gmail.com>
2022-03-24 22:31:23 -0600

    Added forecast HTML + days inside JS

--- script.js
+++ script.js
@@ -1,25 +1,64 @@
-function formatDate(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = "0${hours}";
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = "0${minutes}";
-  }
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
+let now = new Date();
+let h2 = document.querySelector("h2");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+if(hours < 10) {
+  hours = `0${hours}`;
 }
+let minutes = now.getMinutes();
+if(minutes < 10) {
+  minutes = `0${minutes}`;
+}
+h2.innerHTML = `${day} ${hours}:${minutes}`;
+
+
+
+function displayForecast() {
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+   let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
+
+
+
+
+
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -34,12 +73,14 @@ function displayTemperature(response) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
+  humidityElement.innerHTML = `${response.data.main.humidity}%`;;
+  windElement.innerHTML = `${Math.round(response.data.wind.speed)} k/h`;;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   icon.setAttribute(
     "src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
    icon.setAttribute("alt", response.data.weather[0].description);
+
+   displayForecast();
 }
 
 function searchCity(city) {
