52cfbb487782c4f43601ab3edb0cc77fb92b1381
Via Perez <viacamilleperez@gmail.com>
2022-03-24 23:00:17 -0600

    Added forecast API and adjusted F/C toggle

--- script.js
+++ script.js
@@ -48,7 +48,8 @@ function formatDay(timestamp) {
   return days[day];
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
 
   let forecastElement = document.querySelector("#forecast");
 
@@ -81,7 +82,12 @@ function displayForecast() {
 
 
 
-
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "91a6c6c7bba3340ed4c344d1e0ad5434";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 
 
@@ -106,7 +112,7 @@ function displayTemperature(response) {
     "src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
    icon.setAttribute("alt", response.data.weather[0].description);
 
-   displayForecast();
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
